db72bdd2140334a7faf4e982d96db51c82b3dbb2
mayank185T9 <43036566+mayank185T9@users.noreply.github.com>
2018-10-27 12:28:39 +0530

    Update crossofvectors.py

--- crossofvectors.py
+++ crossofvectors.py
@@ -6,9 +6,14 @@ Created on Fri Oct 19 18:01:03 2018
 @author: mayank
 """
 
-p1=list((float(input('x1=')), float(input('y1=')), float(input('z1='))))
-p2=list((float(input('x2=')), float(input('y2=')), float(input('z2='))))
-p3=list((float(input('x3=')), float(input('y3=')), float(input('z3='))))
+x1,y1,z1=eval(input('enter 1st point '))
+p1=[x1,y1,z1]
+
+x2,y2,z2=eval(input('enter 2nd point '))
+p2=[x2,y2,z2]
+
+x3,y3,z3=eval(input('enter 3rd point '))
+p3=[x3,y3,z3]
 
 v1= list(((float(p1[0]-p2[0])),(float(p1[1]-p2[1])) ,(float(p1[2]-p2[2]))))
 v2= list(((float(p1[0]-p3[0])),(float(p1[1]-p3[1])) ,(float(p1[2]-p3[2]))))
